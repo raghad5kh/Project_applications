7ef88af268732396590bde3616374084f044e6a8
Touka26 <toukadaffodil@gmail.com>
2023-11-21 20:54:17 +0200

    edit auth

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Group;
+use Illuminate\Http\Request;
+
+class GroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+        ]);
+
+        $user = auth()->user(); // Get the authenticated user
+
+        // Create the group with fk_admin_id set to the user's ID
+        $group = Group::query()->create([
+            'name' => $data['name'],
+            'fk_admin_id' => $user->id,
+        ]);
+
+        return response()->json(['message' => 'Group created successfully', 'group' => $group]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,14 @@ use Illuminate\Support\Facades\Route;
 
 //Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'user']);
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class,'login']);
-Route::post('/logout', [AuthController::class,'logout']);
+//Route::post('/register', [AuthController::class, 'register']);
+//Route::post('/login', [AuthController::class,'login']);
+//Route::post('/logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
+
+Route::prefix('auth')->group(function () {
+
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class,'login']);
+    Route::post('/logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
+
+});
