47580e7628d9f06e8033d8a0b87eb13ad13b9420
RamaMahjoub <ramamhrama093@gmail.com>
2024-01-05 14:49:06 +0300

    refactor auth service

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Services\Authentication;
+use App\Services\AuthService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
@@ -12,45 +12,49 @@ use Illuminate\Validation\ValidationException;
 class AuthController extends Controller
 {
 
-
-    protected $authenService;
-
-    public function __construct(Authentication $authenService)
-    {
-        $this->authenService = $authenService;
-    }
+    public function __construct(private AuthService $authenService){}
 
     public function register(Request $request)
     {
-
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:8',
             'username' => 'required'
         ]);
-        $register = $this->authenService->register($request);
+        $result = $this->authenService->register($request);
 
-        return $register;
+        return response()->json(['message' => $result['message'], 'user' => $result['user'], 'Token' => $result['authToken']], 200);
 
     }
 
     public function login(Request $request)
     {
-        $request->validate([
+        $credentials = $request->validate([
             'email' => 'required|email',
             'password' => 'required',
         ]);
-        $login = $this->authenService->login($request);
-        return $login;
+        // Use 'email' and 'password' keys in the Auth::attempt method
+        if (!Auth::attempt(['email' => $credentials['email'], 'password' => $credentials['password']])) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+        // Get the authenticated user
+        $user = Auth::user();
+        $result = $this->authenService->login($user);
+        return response()->json(['message' => $result['message'], 'user' => $result['user'], 'Token' => $result['authToken']], 200);
 
 
     }
 
     public function logout(Request $request)
     {
-        $logout = $this->authenService->logout($request);
-        return $logout;
+        $result = $this->authenService->logout($request->user());
+        if (isset($result['status'])) {
+            return response()->json(['message' => $result['message']], $result['status']);
+        }
+        return response()->json(['message' => $result['message']], 200);
     }
 
 }
